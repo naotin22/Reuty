1f3904406bd3a7571d9293947db81bbc43c63821
3410 <3410@example.com>
2024-10-15 15:08:28 +0900

    テーブルの関連付け改3

--- db/migrate/20241015060648_create_reports.rb
+++ db/migrate/20241015060648_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
       t.integer :product_id
-      t.integer :user.id
+      t.integer :user_id
       t.string :report_comment
 
       t.timestamps
