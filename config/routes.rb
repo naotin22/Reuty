988d9fe1c4b21a065fa8910622cf5a6b66295780
3410 <3410@example.com>
2024-10-18 12:48:24 +0900

    product_detailの追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources :purchases
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home" => "home#index"
+  get "product_detail" => "product_detail#index"
+  
 end
