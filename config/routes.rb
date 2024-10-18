275b9babd9909254dbc97184669795dadd566a22
3410 <3410@example.com>
2024-10-18 12:42:48 +0900

    homeの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :products
   resources :purchases
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "home" => "home#index"
 end
