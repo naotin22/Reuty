ce50afd33e7835c8351e1444893777870928ca90
3410 <3410@example.com>
2024-11-01 12:32:13 +0900

    ホーム画面から商品詳細に行けるようにした 商品詳細画面は質問の前まで作った

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def index
     @products = Product.all
     @product_classes = ProductClass.all
+
+    @current_user = current_user
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,18 @@ Rails.application.routes.draw do
   get "home" => "home#index"
   get "product_detail/:id", to: "product_detail#show", as: :product_detail
   get "product_detail" => "product_detail#index"
-  get "put_list" => "put_list#index"
+  get "put_list/:id", to: "put_list#index", as: :put_list
+
   get "put_entry" => "put_entry#index"
   get "product_manage" => "product_manage#index"
   get "like" => "like#index"
+  # get "like/:id", to: "like#show", as: :like
+
   get "make_account" => "make_account#index"
   get "login" => "login#index"
-  get "customer_report" => "customer_report#index"
+
+  get "customer_report", to: "customer_report#index", as: :customer_report
+
   get "manage" => "manage#index"
   get "report_completed" => "report_completed#index"
   get "report_list" => "report_list#index"
