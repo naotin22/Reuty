9d748d730bd08457b428642923c33de0a0a58182
3431 <3431@example.com>
2024-11-13 11:58:17 +0900

    通報一覧（仮）

--- app/controllers/report_list_controller.rb
+++ app/controllers/report_list_controller.rb
@@ -1,7 +1,18 @@
 class ReportListController < ApplicationController
   
   def index
-    @reports = Report.all
+    # @reports = Report.all
+    @reports= Report.order(:user_id, :product_id)
   end
+  def create
+    @reports = Report.all
+    report_list = Report.find_by(user_id: @reports.comment)
+
+    
+
+  end  
+
+
+
 
 end
