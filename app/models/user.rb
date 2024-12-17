098ddfd55ae8354b971d0fa08a83c944653937a2
3431 <3431@example.com>
2024-12-17 13:43:54 +0900

    make_accountのvalidatesです

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :purchases
   has_many :reports
+
+  validates :name, presence: {message:"名前が入力されていません"}
+  validates :password, presence: {message:"パスワードが入力されていません"}
+  validates :mail, presence: {message:"メールアドレスが入力されていません"}
+  validates :mail, uniqueness: { scope: :mail, message:"このメールアドレスは既に登録されています" }
+  validates :address, presence: {message:"住所が入力されていません"}
 end
